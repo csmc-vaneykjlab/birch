746790a5adcef99483ed2f3c6251d91c144eaa8f
bhatarchanas <bhatarchanas@yahoo.com>
2022-08-08 16:47:53 -0500

    added init.r

---
+++ init.r
@@ -0,0 +1,22 @@
+## Install R packages
+packages <- c("shiny", "shinyjs", "shinyalert", "shinyvalidate", "shinythemes", "shinycssloaders", "rsconnect", "DT", "tidyr", "textreadr", "optparse", "dplyr", "tibble", "ggplot2", "assertthat", "readr", "purrr", "tidyverse", "ggpubr", "ColorBrewer", "gtable", "grid", "gridExtra", "sqldf", "tools", "missRanger", "plotly")
+install_if_missing <- function(p) {
+    if (!p %in% rownames(installed.packages())) {
+        install.packages(p)
+    }
+}
+invisible(sapply(packages, install_if_missing))
+
+packages_bio <- c("DEP", "proBatch", "pcaMethods", "ComplexHeatmap", "SummarizedExperiment")
+install_if_missing2 <- function(h) {
+    if (!require("BiocManager", quietly = TRUE)) {}
+        install.packages("BiocManager")
+        if (!h %in% rownames(installed.packages())) {
+            BiocManager::install(h)
+        }
+    }
+}
+invisible(sapply(packages_bio, install_if_missing2))
+
+
+
