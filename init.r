e7a58f0d99e96499e6e25106e2a3c04406d23526
bhatarchanas <bhatarchanas@yahoo.com>
2022-08-09 17:42:50 -0500

    init packages part1

--- init.r
+++ init.r
@@ -1,17 +1,17 @@
 ## Install R packages
-# packages <- c("shiny", "shinyjs", "shinyalert", "shinyvalidate", "shinythemes", "shinycssloaders", "rsconnect", "DT", "tidyr", "textreadr")
+packages <- c("shiny", "shinyjs", "shinyalert", "shinyvalidate", "shinythemes", "shinycssloaders", "rsconnect", "DT", "tidyr", "textreadr")
 # packages <- c("optparse", "dplyr", "tibble", "ggplot2", "assertthat", "readr", "purrr", "tidyverse")
 #packages <- c("ggpubr", "ColorBrewer", "gtable", "grid", "gridExtra", "sqldf", "tools", "missRanger", "plotly")
-#install_if_missing <- function(p) {
-#    print(p)
-#    if (!p %in% rownames(installed.packages())) {
-#        install.packages(p, clean=TRUE, quiet=TRUE)
-#    }
-#}
-#invisible(sapply(packages, install_if_missing))
+install_if_missing <- function(p) {
+    print(p)
+    if (!p %in% rownames(installed.packages())) {
+        install.packages(p, clean=TRUE, quiet=TRUE)
+    }
+}
+invisible(sapply(packages, install_if_missing))
 
 #packages_bio <- c("DEP", "proBatch", "pcaMethods")
-packages_bio <- c("ComplexHeatmap", "SummarizedExperiment")
+#packages_bio <- c("ComplexHeatmap", "SummarizedExperiment")
 install_if_missing2 <- function(h) {
     if (!require("BiocManager", quietly = TRUE)) {
         install.packages("BiocManager")
