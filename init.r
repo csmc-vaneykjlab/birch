35a23e21717327ae0900f9be74fe8ac745522b64
bhatarchanas <bhatarchanas@yahoo.com>
2022-08-09 15:24:44 -0500

    init packages in batches

--- init.r
+++ init.r
@@ -1,22 +1,25 @@
 ## Install R packages
-packages <- c("shiny", "shinyjs", "shinyalert", "shinyvalidate", "shinythemes", "shinycssloaders", "rsconnect", "DT", "tidyr", "textreadr", "optparse", "dplyr", "tibble", "ggplot2", "assertthat", "readr", "purrr", "tidyverse", "ggpubr", "ColorBrewer", "gtable", "grid", "gridExtra", "sqldf", "tools", "missRanger", "plotly")
+# "shiny", "shinyjs", "shinyalert", "shinyvalidate", "shinythemes", "shinycssloaders", "rsconnect", "DT", "tidyr", "textreadr"
+packages <- c("optparse", "dplyr", "tibble", "ggplot2", "assertthat", "readr", "purrr", "tidyverse", "ggpubr", "ColorBrewer")
+# "gtable", "grid", "gridExtra", "sqldf", "tools", "missRanger", "plotly"
 install_if_missing <- function(p) {
+    print(p)
     if (!p %in% rownames(installed.packages())) {
         install.packages(p, clean=TRUE, quiet=TRUE)
     }
 }
 invisible(sapply(packages, install_if_missing))
 
-packages_bio <- c("DEP", "proBatch", "pcaMethods", "ComplexHeatmap", "SummarizedExperiment")
-install_if_missing2 <- function(h) {
-    if (!require("BiocManager", quietly = TRUE)) {}
-        install.packages("BiocManager")
-        if (!h %in% rownames(installed.packages())) {
-            BiocManager::install(h)
-        }
-    }
-}
-invisible(sapply(packages_bio, install_if_missing2))
+#packages_bio <- c("DEP", "proBatch", "pcaMethods", "ComplexHeatmap", "SummarizedExperiment")
+#install_if_missing2 <- function(h) {
+#    if (!require("BiocManager", quietly = TRUE)) {}
+#        install.packages("BiocManager")
+#        if (!h %in% rownames(installed.packages())) {
+#            BiocManager::install(h)
+#        }
+#    }
+#}
+#invisible(sapply(packages_bio, install_if_missing2))
 
 
 
