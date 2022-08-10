b1baa38cddc5c7b6650085c905522581d45adc91
bhatarchanas <bhatarchanas@yahoo.com>
2022-08-10 11:36:12 -0500

    rerunning probatch install

--- init.r
+++ init.r
@@ -1,26 +1,26 @@
 ## Install R packages
 #packages <- c("shiny", "shinyjs", "shinyalert", "shinyvalidate", "shinythemes", "shinycssloaders", "rsconnect", "DT", "tidyr", "textreadr")
 #packages <- c("optparse", "dplyr", "tibble", "ggplot2", "assertthat", "readr", "purrr", "tidyverse")
-packages <- c("ggpubr", "RColorBrewer", "gtable", "grid", "gridExtra", "sqldf", "tools", "missRanger", "plotly")
-install_if_missing <- function(p) {
-    print(p)
-    if (!p %in% rownames(installed.packages())) {
-        install.packages(p, clean=TRUE, quiet=TRUE)
-    }
-}
-invisible(sapply(packages, install_if_missing))
-
-#packages_bio <- c("DEP", "proBatch", "pcaMethods")
-#packages_bio <- c("ComplexHeatmap", "SummarizedExperiment")
-#install_if_missing2 <- function(h) {
-#    if (!require("BiocManager", quietly = TRUE)) {
-#        install.packages("BiocManager")
-#        if (!h %in% rownames(installed.packages())) {
-#            BiocManager::install(h, clean=TRUE, quiet=TRUE)
-#        }
+#packages <- c("ggpubr", "RColorBrewer", "gtable", "grid", "gridExtra", "sqldf", "tools", "missRanger", "plotly")
+#install_if_missing <- function(p) {
+#    print(p)
+#    if (!p %in% rownames(installed.packages())) {
+#        install.packages(p, clean=TRUE, quiet=TRUE)
 #    }
 #}
-#invisible(sapply(packages_bio, install_if_missing2))
+#invisible(sapply(packages, install_if_missing))
 
-install.packages("localpkgs/DEP.tar.gz", repos=NULL, type="source")
+packages_bio <- c("proBatch", "pcaMethods")
+#packages_bio <- c("ComplexHeatmap", "SummarizedExperiment")
+install_if_missing2 <- function(h) {
+    if (!require("BiocManager", quietly = TRUE)) {
+        install.packages("BiocManager")
+        if (!h %in% rownames(installed.packages())) {
+            BiocManager::install(h, clean=TRUE, quiet=TRUE)
+        }
+    }
+}
+invisible(sapply(packages_bio, install_if_missing2))
+
+#install.packages("localpkgs/DEP.tar.gz", repos=NULL, type="source")
 
