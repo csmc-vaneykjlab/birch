ca2428e56b57c3983dded349faacb79fc3ec24d1
bhatarchanas <bhatarchanas@yahoo.com>
2022-08-10 18:46:04 -0500

    all packages

--- init.r
+++ init.r
@@ -1,7 +1,7 @@
 ## Install R packages
 #packages <- c("shiny", "shinyjs", "shinyalert", "shinyvalidate", "shinythemes", "shinycssloaders", "rsconnect", "DT", "tidyr", "textreadr")
-packages <- c("optparse", "dplyr", "tibble", "ggplot2", "assertthat", "readr", "purrr", "tidyverse")
-#packages <- c("ggpubr", "RColorBrewer", "gtable", "grid", "gridExtra", "sqldf", "tools", "missRanger", "plotly")
+#packages <- c("optparse", "dplyr", "tibble", "ggplot2", "assertthat", "readr", "purrr", "tidyverse")
+packages <- c("ggpubr", "RColorBrewer", "gtable", "grid", "gridExtra", "sqldf", "tools", "missRanger", "plotly")
 install_if_missing <- function(p) {
     print(p)
     if (!p %in% rownames(installed.packages())) {
@@ -11,23 +11,23 @@ install_if_missing <- function(p) {
 invisible(sapply(packages, install_if_missing))
 
 # "pcaMethods"
-#packages_bio <- c("ComplexHeatmap", "SummarizedExperiment")
-#install_if_missing2 <- function(h) {
+packages_bio <- c("ComplexHeatmap", "SummarizedExperiment")
+install_if_missing2 <- function(h) {
 #    if (!require("BiocManager", quietly = TRUE)) {
 #        install.packages("BiocManager")
-#        if (!h %in% rownames(installed.packages())) {
-#            print(h)
-#            BiocManager::install(h, clean=TRUE, quiet=TRUE)
-#        }
-#    }
-#}
-#invisible(sapply(packages_bio, install_if_missing2))
-
-
-if (!require("BiocManager", quietly = TRUE)) {
-    install.packages("BiocManager")
     BiocManager::install(version = "3.10")
-    install.packages(c("localpkgs/DEP.tar.gz", "localpkgs/proBatch.tar.gz"), repos=NULL, type="source")
+    if (!h %in% rownames(installed.packages())) {
+        print(h)
+        BiocManager::install(h, clean=TRUE, quiet=TRUE)
+    }
+#   }
 }
+invisible(sapply(packages_bio, install_if_missing2))
+
+
+#if (!require("BiocManager", quietly = TRUE)) {
+#install.packages("BiocManager")
+install.packages(c("localpkgs/DEP.tar.gz", "localpkgs/proBatch.tar.gz"), repos=NULL, type="source")
+#}
 
 
