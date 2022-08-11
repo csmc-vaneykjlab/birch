4a67455ba996abfd9969a6ae39cb5b33c78f8db8
bhatarchanas <bhatarchanas@yahoo.com>
2022-08-11 18:44:48 -0500

    install local with dependencies

--- init.r
+++ init.r
@@ -1,7 +1,7 @@
 ## Install R packages
 #packages <- c("shiny", "shinyjs", "shinyalert", "shinyvalidate", "shinythemes", "shinycssloaders", "rsconnect", "DT", "tidyr", "textreadr")
 #packages <- c("optparse", "dplyr", "tibble", "ggplot2", "assertthat", "readr", "purrr", "tidyverse")
-packages <- c("ggpubr", "RColorBrewer", "gtable", "grid", "gridExtra", "tools", "missRanger", "plotly")
+packages <- c("ggpubr", "RColorBrewer", "gtable", "grid", "gridExtra", "tools", "missRanger", "plotly", "devtools")
 install_if_missing <- function(p) {
     print(p)
     if (!p %in% rownames(installed.packages())) {
