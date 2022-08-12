7e1450480683b4bb9e03c13e54f2255763549344
bhatarchanas <bhatarchanas@yahoo.com>
2022-08-11 19:53:03 -0500

    install local with dependencies

--- init.r
+++ init.r
@@ -1,30 +1,30 @@
 ## Install R packages
 #packages <- c("shiny", "shinyjs", "shinyalert", "shinyvalidate", "shinythemes", "shinycssloaders", "rsconnect", "DT", "tidyr", "textreadr")
 #packages <- c("optparse", "dplyr", "tibble", "ggplot2", "assertthat", "readr", "purrr", "tidyverse")
-packages <- c("ggpubr", "RColorBrewer", "gtable", "grid", "gridExtra", "tools", "missRanger", "plotly")
-install_if_missing <- function(p) {
-    print(p)
-    if (!p %in% rownames(installed.packages())) {
-        install.packages(p, clean=TRUE, quiet=TRUE)
-    }
-}
-invisible(sapply(packages, install_if_missing))
+#packages <- c("ggpubr", "RColorBrewer", "gtable", "grid", "gridExtra", "tools", "missRanger", "plotly")
+#install_if_missing <- function(p) {
+#    print(p)
+#    if (!p %in% rownames(installed.packages())) {
+#        install.packages(p, clean=TRUE, quiet=TRUE)
+#    }
+#}
+#invisible(sapply(packages, install_if_missing))
 
-packages_bio <- c("pcaMethods", "SummarizedExperiment")
-install_if_missing2 <- function(h) {
-    if (!require("BiocManager", quietly = TRUE)) {
-       install.packages("BiocManager")
-    }
-    BiocManager::install(version = "3.10")
-    if (!h %in% rownames(installed.packages())) {
-        print(h)
-        BiocManager::install(h, clean=TRUE, quiet=TRUE)
-    }
-}
-invisible(sapply(packages_bio, install_if_missing2))
+#packages_bio <- c("pcaMethods", "SummarizedExperiment")
+#install_if_missing2 <- function(h) {
+#    if (!require("BiocManager", quietly = TRUE)) {
+#       install.packages("BiocManager")
+#    }
+#    BiocManager::install(version = "3.10")
+#    if (!h %in% rownames(installed.packages())) {
+#        print(h)
+#        BiocManager::install(h, clean=TRUE, quiet=TRUE)
+#    }
+#}
+#invisible(sapply(packages_bio, install_if_missing2))
 
 
-install.packages(c("localpkgs/devtools_2.4.4.tar.gz")
+install.packages("localpkgs/devtools_2.4.4.tar.gz", repos=NULL, type="source")
 
 
 #if (!require("BiocManager", quietly = TRUE)) {
