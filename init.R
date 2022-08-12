811f61a9a4bce9066a0669784d3f42f6e29d0ba8
bhatarchanas <bhatarchanas@yahoo.com>
2022-08-12 13:43:07 -0500

    packages with renv

---
+++ init.R
@@ -0,0 +1,9 @@
+my_packages = c("renv")
+
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p, clean=TRUE, quiet=TRUE)
+  }
+}
+
+invisible(sapply(my_packages, install_if_missing))
