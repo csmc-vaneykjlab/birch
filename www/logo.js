0753394f7e6700cab1d8dc272152512e89c573f3
bhatarchanas <bhatarchanas@yahoo.com>
2022-10-03 11:46:26 -0500

    adding logo

--- www/logo.js
+++ www/logo.js
@@ -1,3 +1,3 @@
 $( document ).ready(function() {
-  $( ".navbar .container-fluid" ).append( '<img src="birch.PNG" align="right" width=100 height=80>' );
+  $( ".navbar .container-fluid" ).append( '<img src="birch.png" align="right" width=100 height=100>' );
 });
