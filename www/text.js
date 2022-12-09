78ea426ff256b4ecc95480b857882d2089392c81
zachdwight <zachdwight@yahoo.com>
2022-12-09 09:24:46 -0800

    Update text.js
    EMPTY due to security issues

--- www/text.js
+++ www/text.js
@@ -1,11 +1,1 @@
-window.pressed = function(){
-        var a = document.getElementById('fileIn');
-        if(a.value === "")
-        {
-            noFile.innerHTML = "No folder choosen";
-        }
-        else
-        {
-            noFile.innerHTML = JSON.stringify(a.value);
-        }
-    };
+
