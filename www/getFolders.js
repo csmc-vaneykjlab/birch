61d13190a0722540f6cf35ba71e66381c5a83b59
zachdwight <zachdwight@yahoo.com>
2022-12-09 09:24:21 -0800

    Update getFolders.js
    EMPTY

--- www/getFolders.js
+++ www/getFolders.js
@@ -1,9 +1,1 @@
-document.getElementById("fileIn").addEventListener("change", function(e) {
 
-        let files = e.target.files;
-        var relativePath = files[0].webkitRelativePath;
-        var folder = relativePath.split("/");
-
-        Shiny.onInputChange("mydata", folder[0]);
-
-});
