abef845f2a313197bf912a77a7afbace4c6fb298
bhatarchanas <bhatarchanas@yahoo.com>
2022-12-12 11:18:41 -0600

    Changed workflow image

--- www/getFolders.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("fileIn").addEventListener("change", function(e) {
-
-        let files = e.target.files;
-        var relativePath = files[0].webkitRelativePath;
-        var folder = relativePath.split("/");
-
-        Shiny.onInputChange("mydata", folder[0]);
-
-});
